518c8d23174ead9d64fbe05ee2e4eacd4698b4aa
Ramachandra-2k96 <ramachandraudupa.sangamone@gmail.com>
2024-06-28 00:35:48 +0530

    update

--- app1/urls.py
+++ app1/urls.py
@@ -1,9 +1,9 @@
 # urls.py
 from django.urls import path
-from .views import upload_file,search_movies,land,login_view,logout_view,home,movie_detail,profile,add,search
+from .views import search_movies,land,login_view,logout_view,home,movie_detail,profile,add,search#,upload_file
 
 urlpatterns = [
-    path('upload/', upload_file, name='upload_file'),
+    #path('upload/', upload_file, name='upload_file'),
     path('', land, name='land'),
     path('home', home, name='home'),
     path('out', logout_view, name='logout'),
